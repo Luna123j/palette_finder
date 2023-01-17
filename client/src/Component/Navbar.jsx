543a04583debbcbb769c12a2f4290673fe31138f
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-17 16:20:04 -0500

    add navbar using bootstrap

---
+++ client/src/Component/Navbar.jsx
@@ -0,0 +1,24 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+function NavBar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">ABC LOGO</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Nav className="justify-content-end">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Navbar.Text>
+              Signed in as: <a href="#login">Mark Otto</a>
+            </Navbar.Text>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
