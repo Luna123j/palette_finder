20feb32dde9b03a547171c71c61cac11ba408742
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-24 17:58:06 -0500

    add user register route

--- server/app/controllers/users_controller.rb
+++ server/app/controllers/users_controller.rb
@@ -3,4 +3,25 @@ class UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
+
+  # render view for register
+  def new
+  end
+
+  #create new user
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      redirect_to '/signup'
+    end
+  end
+
+    private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+
 end
