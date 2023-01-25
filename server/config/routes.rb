a127d478bdb3377044fb44e5d119232de5c1f4f6
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-25 18:45:23 -0500

    add sessions controller

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   get 'users/index'
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   get 'signup' => 'users#new'
   post '/users' => 'users#create'
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
