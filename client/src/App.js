fc50f73373fdd2bdd9299c21aaed3abdf11f17d0
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-01-30 23:33:20 -0500

    set login states

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,29 @@ import NavBar from './Component/Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from './Component/Login';
 import Signup from './Component/Signup';
+import { useSelector, useDispatch } from 'react-redux'
+import { loggedin, reset } from './redux/isLoggedIn';
 import Home from './Component/Home';
+import { useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    axios.get('/islogin', { withCredentials: true })
+      .then(res => {
+        console.log(res.data)
+        if (res.data.logged_in) {
+          dispatch({ type: "isLoggedIn/loggedin", payload: res.data.user });
+        } else {
+          dispatch(reset())
+        }
+      })
+      .catch(error => console.log('api errors:', error))
+  }, [])
+
   return (
     <BrowserRouter>
       <NavBar />
