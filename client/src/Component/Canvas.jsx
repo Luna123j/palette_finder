ab8bdb661aee1a6ed00ccc8271f32b06b43a9e62
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-02-09 19:24:10 -0500

    generate palette

--- client/src/Component/Canvas.jsx
+++ client/src/Component/Canvas.jsx
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from "react";
+import {buildRgb,quantization} from "./helpers/generatePalette.js"
 
 export default function Canvas(props) {
   const canvasRef = useRef(null)
@@ -17,6 +18,9 @@ export default function Canvas(props) {
         ctx.drawImage(image, 0, 0, ctx.canvas.width, ctx.canvas.height);
         const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
         console.log('this is image data',imageData)
+        const rgbValues=buildRgb(imageData.data);
+        const colors = quantization(rgbValues,2)
+        console.log(colors)
       }
     }
     
