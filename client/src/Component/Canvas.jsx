67c46957ff7ef3d4d4c21e26052e432b9f94aeb8
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-02-10 20:30:46 -0500

    edit

--- client/src/Component/Canvas.jsx
+++ client/src/Component/Canvas.jsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from "react";
-import { buildRgb, quantization } from "./helpers/generatePalette.js"
-import { useDispatch,useSelector } from "react-redux";
+import { buildRgb, quantization, hex } from "./helpers/generatePalette.js"
+import { useDispatch, useSelector } from "react-redux";
 
 export default function Canvas(props) {
   const dispatch = useDispatch();
@@ -9,27 +9,42 @@ export default function Canvas(props) {
   const imgUrl = props.imgurl;
   let colors;
   console.log('this is image file', imgFile)
+  console.log('this is image url file', imgUrl)
+
 
   useEffect(() => {
     const canvas = canvasRef.current
     const ctx = canvas.getContext('2d')
     const image = new Image();
     image.src = imgUrl;
+    image.crossOrigin = "Anonymous";
+  
     image.onload = () => {
 
-      if (imgFile.length > 0) {
+      if (imgUrl !== undefined) {
         ctx.drawImage(image, 0, 0, ctx.canvas.width, ctx.canvas.height);
         const imageData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height);
         console.log('this is image data', imageData)
+        // dispatch({type: 'imgInfo/setImgFile', payload: imageData})
         const rgbValues = buildRgb(imageData.data);
-        colors = quantization(rgbValues, 2)
+        const colorDataInRgb = quantization(rgbValues, 2)
+        colors = colorDataInRgb
         console.log(colors)
-        dispatch({type: 'paletteData/setPaletteData', payload: colors})
+        const colorDataInHex = colors.map((color) => {
+          return ("#" + hex(color.r) + hex(color.g) + hex(color.b)).toUpperCase();
+        })
+        console.log(colorDataInHex)
+        dispatch({ type: 'paletteData/setPaletteDataRgb', payload: colorDataInRgb })
+        dispatch({ type: 'paletteData/setPaletteDataHex', payload: colorDataInHex })
+
+        const dataURL = canvas.toDataURL();
+        console.log('this is canva data url',dataURL);
+        dispatch({ type: "imgInfo/setImgUrl", payload: `${dataURL}` });
       }
     }
 
   }, [imgUrl])
-  
+
 
   // const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   return <canvas ref={canvasRef} {...props} />
