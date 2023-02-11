ee758fd375dfe5688c98c70b7bfbecec73305f2a
Luna123j <58311487+Luna123j@users.noreply.github.com>
2023-02-10 20:28:16 -0500

    remove function

--- client/src/Component/ColorBox.jsx
+++ client/src/Component/ColorBox.jsx
@@ -3,16 +3,10 @@ import React from "react";
 export default function ColorBox(props) {
 
   console.log(props)
-  const color = props.color;
-
-  function hex(x) {
-    return ("0" + parseInt(x).toString(16)).slice(-2);
-  }
-
-  const colorInHex = "#" + hex(color.r) + hex(color.g) + hex(color.b)
+  const colorInHex = props.color;
 
   return (
-    <div key={props.index} style={{
+    <div style={{
       backgroundColor: `${colorInHex}`,
       width: '100px', height: '100px'
     }}> {colorInHex}</div>
